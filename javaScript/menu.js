0b7f7b9051b711a4002c05dcda40a57365b67d5c
huichuan1999 <58366430+huichuan1999@users.noreply.github.com>
2023-12-01 00:11:45 +0000

    Add WECHAT QRCode，Add Side bar

--- javaScript/menu.js
+++ javaScript/menu.js
@@ -1,8 +1,24 @@
 document.addEventListener('DOMContentLoaded', function() {
     var hamburger = document.querySelector('.hamburger-menu');
-    var menu = document.querySelector('.menu');
+    var sidebar = document.querySelector('.sidebar');
 
-    hamburger.addEventListener('click', function() {
-        menu.classList.toggle('active');
+    // 打开或关闭侧边栏
+    function toggleSidebar() {
+        sidebar.style.left = sidebar.style.left === '0px' ? '-200px' : '0px';
+    }
+
+    // 点击汉堡菜单时切换侧边栏状态
+    hamburger.addEventListener('click', function(event) {
+        toggleSidebar();
+        event.stopPropagation(); // 阻止事件冒泡
+    });
+
+    // 点击页面其他地方时关闭侧边栏
+    document.addEventListener('click', function(event) {
+        var isClickInside = hamburger.contains(event.target) || sidebar.contains(event.target);
+
+        if (!isClickInside && sidebar.style.left === '0px') {
+            toggleSidebar();
+        }
     });
 });
